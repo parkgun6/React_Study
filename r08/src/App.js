d22874ec626acb2f6a552950c8b1a489955edb79
parkgun6 <76540460+parkgun6@users.noreply.github.com>
2021-05-27 18:23:10 +0900

    Toolkit을 사용한 Counter, Movie카드 예제

--- r08/src/App.js
+++ r08/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import CountDisplay from './Counter/CountDisplay';
 import CountButtons from './Counter/CountButtons';
+import MovieBoard from './Movie/MovieBoard';
 
 function App() {
   return (
     <div>
+      {/* <CountDisplay></CountDisplay>
       <CountDisplay></CountDisplay>
       <CountDisplay></CountDisplay>
-      <CountDisplay></CountDisplay>
-      <CountButtons></CountButtons>
+      <CountButtons></CountButtons> */}
+
+      <MovieBoard></MovieBoard>
     </div>
   );
 }
